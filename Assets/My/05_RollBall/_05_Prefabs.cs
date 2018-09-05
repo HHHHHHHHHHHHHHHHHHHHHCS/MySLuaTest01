e5e529c1b57dcea88b173587ab43b8ca6c0d1d11
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-09-05 20:58:11 +0800

    小球场景的搭建

---
+++ Assets/My/05_RollBall/_05_Prefabs.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _05_Prefabs : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject uiPrefab;
+    [SerializeField]
+    private GameObject worldPrefab;
+    [SerializeField]
+    private GameObject foodPrefab;
+    [SerializeField]
+    private GameObject playerPrefab;
+}
