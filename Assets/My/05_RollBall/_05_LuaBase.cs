6c1ac2d4d9db5204ef93205bb77bfc18f84b46ef
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-09-06 14:12:49 +0800

    roll a ball 大致的框架

---
+++ Assets/My/05_RollBall/_05_LuaBase.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SLua;
+using System;
+
+[CustomLuaClass]
+public class _05_LuaBase : MonoBehaviour
+{
+    [SerializeField]
+    private string luaPath;
+
+    private LuaTable table;
+    private Action update;
+
+    private void Awake()
+    {
+        if (string.IsNullOrEmpty(luaPath))
+        {
+            return;
+        }
+
+        LuaSvr luaSvr = new LuaSvr();
+        luaSvr.init(null, () =>
+         {
+             table = (LuaTable)luaSvr.start(luaPath);
+             update = ((LuaFunction)table["Update"]).cast<Action>();
+         });
+    }
+
+    private void Update()
+    {
+        return;
+        if(update!=null)
+        {
+            update();
+        }
+        else
+        {
+            Debug.Log("update is null");
+        }
+    }
+
+}
